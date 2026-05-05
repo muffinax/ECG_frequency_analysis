c2d0b713a9be915ed261f43033f080fee2578ad4
PoProstuFilip14 <f.romanowski@hotmail.com>
2026-05-05 22:35:33 +0200

    Revert "Improved square and color detection"
    This reverts commit 35ae48ec46698a23e9fb121a662a57751825b78e.

--- machine_learning/model.py
+++ machine_learning/model.py
@@ -2,12 +2,19 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout, BatchNormalization
 
-time_steps = 100
-num_features = 64
-num_labels = 3
+# ------------------------
+# PARAMETRY
+# ------------------------
+time_steps = 100  # długość sekwencji (np. liczba okien FFT)
+num_features = 64  # liczba częstotliwości
+num_labels = 3  # liczba chorób
 
+# ------------------------
+# MODEL
+# ------------------------
 model = Sequential([
 
+    # LSTM - analizuje sekwencję widm
     LSTM(128, return_sequences=True, input_shape=(time_steps, num_features)),
     BatchNormalization(),
     Dropout(0.3),
@@ -16,14 +23,20 @@ model = Sequential([
     BatchNormalization(),
     Dropout(0.3),
 
+    # warstwa gęsta
     Dense(32, activation='relu'),
 
+    # 🔥 KLUCZ MULTILABEL
     Dense(num_labels, activation='sigmoid')
 ])
 
+# ------------------------
+# KOMPILACJA
+# ------------------------
 model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
 
+    # 🔥 KLUCZ MULTILABEL
     loss='binary_crossentropy',
 
     metrics=[
