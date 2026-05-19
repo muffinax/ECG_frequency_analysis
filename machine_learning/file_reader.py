837580dd140870ee96546885f74afe20989ea729
PoProstuFilip14 <f.romanowski@hotmail.com>
2026-05-19 23:08:36 +0200

    reading file with mld

--- machine_learning/file_reader.py
+++ machine_learning/file_reader.py
@@ -7,12 +7,12 @@ from file_manager import InputManagerException
 
 print("Reading file...")
 
-if not os.path.exists("C:/Users/froma/Downloads/mit-bih-arrhythmia-database-1.0.0-20260518T190848Z-3-001/mit-bih-arrhythmia-database-1.0.0/102/102.mldat"):
-    raise FileNotFoundError("C:/Users/froma/Downloads/mit-bih-arrhythmia-database-1.0.0-20260518T190848Z-3-001/mit-bih-arrhythmia-database-1.0.0/102/102.mldat")
+if not os.path.exists("C:/Users/froma/Downloads/mit-bih-arrhythmia-database-1.0.0-20260518T190848Z-3-001/mit-bih-arrhythmia-database-1.0.0/107/107.mldat"):
+    raise FileNotFoundError("C:/Users/froma/Downloads/mit-bih-arrhythmia-database-1.0.0-20260518T190848Z-3-001/mit-bih-arrhythmia-database-1.0.0/107/107.mldat")
 
 try:
     print("Start read()")
-    with open("C:/Users/froma/Downloads/mit-bih-arrhythmia-database-1.0.0-20260518T190848Z-3-001/mit-bih-arrhythmia-database-1.0.0/102/102.mldat", 'rb') as f:
+    with open("C:/Users/froma/Downloads/mit-bih-arrhythmia-database-1.0.0-20260518T190848Z-3-001/mit-bih-arrhythmia-database-1.0.0/107/107.mldat", 'rb') as f:
         records: list[dict[str, Any]] = pickle.load(f)
         print("Loaded records:", type(records), len(records))
 
@@ -23,6 +23,7 @@ try:
             signal_sample_index_start=rec['signal_sample_index_start'],
             signal_duration=rec['signal_duration'],
             signal_name=rec['signal_name'],
+            signal_fft_freqs=rec['signal_fft_freqs'],
             signal_fft=rec['signal_fft'],
             signal_sampling_frequency=rec['signal_sampling_frequency'],
             annotations=rec.get('annotations')
